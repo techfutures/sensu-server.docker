994dcf4c1ba38bbff8e17a94e3ed7528b685db31
Andrew Gee <agee@techfutures.co>
2015-01-15 20:14:22 -0800

    modified ENV ports for tutum

--- run.sh
+++ run.sh
@@ -18,14 +18,14 @@ cat << EOF > /etc/sensu/config.json
       "private_key_file": "/ssl/key.pem"
     },
     "host": "$RABBITMQ_HOST",
-    "port": $RABBITMQ_PORT,
+    "port": $RABBITMQ_PORT_15672_TCP_PORT,
     "vhost": "$RABBITMQ_VHOST",
     "user": "$RABBITMQ_USER",
     "password": "$RABBITMQ_PASS"
   },
   "redis": {
     "host": "$REDIS_HOST",
-    "port": $REDIS_PORT
+    "port": $REDIS_PORT_6379_TCP_PORT
   }
 }
 EOF
